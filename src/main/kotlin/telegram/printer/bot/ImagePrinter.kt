bbf3f4e7db01d9b689f7ee7c4c5a8920ac0cd962
seko0716 <sergey.kosov@netcracker.com>
2022-01-01 15:38:31 +0400

    refactoring

--- src/main/kotlin/telegram/printer/bot/ImagePrinter.kt
+++ src/main/kotlin/telegram/printer/bot/ImagePrinter.kt
@@ -1,40 +1,32 @@
 package telegram.printer.bot
 
-import java.awt.image.BufferedImage
-import java.awt.print.Printable
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.pdmodel.PDPage
+import org.apache.pdfbox.pdmodel.PDPageContentStream
+import org.apache.pdfbox.pdmodel.common.PDRectangle
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject
 import java.io.ByteArrayInputStream
 import javax.imageio.ImageIO
-import kotlin.math.ceil
-import kotlin.math.max
 
 class ImagePrinter : Printer() {
-    companion object {
-        // https://www.papersizes.org/a-sizes-in-pixels.htm
-        const val width = 595
-        const val height = 842
-    }
-
     override fun print(byteArray: ByteArray) {
-        val bufferedImage = ImageIO.read(ByteArrayInputStream(byteArray))
-        val printable = Printable { graphics, pageFormat, pageIndex -> // Get the upper left corner that it printable
-            val x = ceil(pageFormat.imageableX).toInt()
-            val y = ceil(pageFormat.imageableY).toInt()
-            if (pageIndex != 0) {
-                return@Printable Printable.NO_SUCH_PAGE
-            }
-            val (width, height) = calculateSize(bufferedImage)
-            graphics.drawImage(bufferedImage, x, y, width, height, null)
-            Printable.PAGE_EXISTS
-        }
-        print(printable)
+        val pdf = getPdf(byteArray)
+        PdfPrinter().print(pdf)
     }
 
-    private fun calculateSize(image: BufferedImage): Pair<Int, Int> {
-        return if (image.width <= width && image.height <= height) {
-            image.width to image.height
-        } else {
-            val max = max(image.width - width, image.height - height)
-            image.width - max to image.height - max
-        }
+    private fun getPdf(byteArray: ByteArray): PDDocument {
+        val bufferedImage = ImageIO.read(ByteArrayInputStream(byteArray))
+        val document = PDDocument()
+
+        val width = bufferedImage.width.toFloat()
+        val height = bufferedImage.height.toFloat()
+        val page = PDPage(PDRectangle(width, height))
+        document.addPage(page)
+        val img = PDImageXObject.createFromByteArray(document, byteArray, "img")
+        val contentStream = PDPageContentStream(document, page)
+        contentStream.drawImage(img, 0f, 0f)
+        contentStream.close()
+
+        return document
     }
 }
--- src/main/kotlin/telegram/printer/bot/PdfPrinter.kt
+++ src/main/kotlin/telegram/printer/bot/PdfPrinter.kt
@@ -3,7 +3,7 @@ package telegram.printer.bot
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.pdmodel.PDPage
 import org.apache.pdfbox.pdmodel.PDPageContentStream
-import org.apache.pdfbox.pdmodel.PDPageTree
+import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode.PREPEND
 import org.apache.pdfbox.pdmodel.common.PDRectangle
 import org.apache.pdfbox.printing.PDFPrintable
 import org.apache.pdfbox.printing.Scaling
@@ -11,42 +11,46 @@ import org.apache.pdfbox.util.Matrix
 import java.awt.print.Book
 import java.awt.print.PageFormat
 import java.awt.print.Paper
-import java.awt.print.PrinterJob
 
 
 class PdfPrinter : Printer() {
+    private val width: Float = 612.0F
+    private val height: Float = 792.0F
+    private val x: Double = 0.0
+    private val y: Double = 0.0
+
     override fun print(byteArray: ByteArray) {
         val document = PDDocument.load(byteArray)
-        val job: PrinterJob = PrinterJob.getPrinterJob()
-        val tree: PDPageTree = document.documentCatalog.pages
-        val iterator: Iterator<PDPage> = tree.iterator()
-        while (iterator.hasNext()) {
-            val page = iterator.next()
-            if (page.mediaBox.width > 612 || page.mediaBox.height > 792) {
-                val fWidth = 612f / page.mediaBox.width
-                val fHeight = 792f / page.mediaBox.height
-                var factor = 0f
-                factor = if (fWidth > fHeight) {
-                    fHeight
-                } else {
-                    fWidth
-                }
-                val contentStream = PDPageContentStream(
-                    document, page,
-                    PDPageContentStream.AppendMode.PREPEND, false
-                )
+        print(document)
+    }
+
+    fun print(document: PDDocument) {
+        document.documentCatalog.pages.forEach { page ->
+            if (page.mediaBox.width > width || page.mediaBox.height > height) {
+                val factor: Float = getFactor(page)
+                val contentStream = PDPageContentStream(document, page, PREPEND, false)
                 contentStream.transform(Matrix.getScaleInstance(factor, factor))
                 contentStream.close()
                 page.mediaBox = PDRectangle.LETTER
             }
         }
         val paper = Paper()
-        paper.setSize(612.0, 792.0)
-        paper.setImageableArea(0.0, 0.0, 612.0, 792.0)
+        paper.setSize(width.toDouble(), height.toDouble())
+        paper.setImageableArea(x, y, width.toDouble(), height.toDouble())
         val pageFormat = PageFormat()
         pageFormat.paper = paper
         val book = Book()
         book.append(PDFPrintable(document, Scaling.SHRINK_TO_FIT), pageFormat, document.numberOfPages)
         print(book)
+    }
+
+    private fun getFactor(page: PDPage): Float {
+        val fWidth = width / page.mediaBox.width
+        val fHeight = height / page.mediaBox.height
+        return if (fWidth > fHeight) {
+            fHeight
+        } else {
+            fWidth
+        }
     }
 }
